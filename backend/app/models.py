6fbe272678407bbfe952ec0977156eb4c088f1bd
Albert Julio <albertjuliotheodorus@gmail.com>
2023-09-04 19:59:16 +0000

    Added test models.

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Permissions(models.Model):
+    name = models.CharField(max_length=50)
+
+class Roles(models.Model):
+    name = models.CharField(max_length=50)
+    permissions = models.ManyToManyField(Permissions)
