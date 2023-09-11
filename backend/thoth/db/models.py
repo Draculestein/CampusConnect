db6842e892bfb4a681204a3805b4e6c9c7d37fb4
Albert Julio <albertjuliotheodorus@gmail.com>
2023-09-11 02:19:43 +0000

    Added string name for Programs.

--- backend/thoth/db/models.py
+++ backend/thoth/db/models.py
@@ -92,3 +92,6 @@ class Programs(models.Model):
     cip = models.CharField(max_length=4)
     name = models.CharField(max_length=150)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
