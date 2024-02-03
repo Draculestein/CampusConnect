574f5ea9377ad89d8cfa7b20ab4ebd7d9e8f0901
Albert Julio <albertjuliotheodorus@gmail.com>
2024-02-03 15:48:28 -0700

    Deleted Django stuff.

--- backend/thoth/statics/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("form", views.fill_in_form, name="fill_in_form"),
-    path("search", views.search_result, name="search_result")
-]
