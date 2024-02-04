3ee35fc1ef228588730a5d2532a4484e1cf3dfa7
Albert Julio <albertjuliotheodorus@gmail.com>
2024-02-03 23:12:59 -0700

    Tested passing arguments to EJS.

--- routes/result.js
+++ routes/result.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET result */
 router.get('/', function(req, res, next) {
-  res.render('result page');
+  res.render('result page', {
+    content: [{
+        image: '/images/2Q.png',
+        background : ''
+      },
+      {
+        image: '/images/3Q.png',
+        background : ''
+      }
+    ]
+  });
 });
 
 module.exports = router;
