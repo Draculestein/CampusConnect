6b208c88b9fa69426ffd3c3c3586b1a18db20feb
Albert Julio <albertjuliotheodorus@gmail.com>
2024-02-13 01:00:15 -0700

    Setup test typescript express.

--- index.ts
+++ index.ts
@@ -8,17 +8,17 @@ const app = express();
 const port = process.env.PORT || 3000; // default port to listen
 
 // Configure Express to use EJS
-app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" );
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
+app.get("/", (req, res) => {
     // render the index template
-    res.render( "index" );
-} );
+    res.render("index");
+});
 
 // start the express server
-app.listen( port, () => {
+app.listen(port, () => {
     // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+    console.log(`server started at http://localhost:${port}`);
+});
