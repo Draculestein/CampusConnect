7c83d594fee9107d94624bf2dae637cb91d2ae19
Albert Julio <albertjuliotheodorus@gmail.com>
2024-02-13 01:27:16 -0700

    Added test result params.

--- routes/result.ts
+++ routes/result.ts
@@ -1,5 +1,30 @@
 import * as express from "express";
 
 export function register(app: express.Application) {
-
+    app.get('/result', function(req, res, next) {
+        res.render('result page', {
+          content: [
+            {
+              image: '/images/Harvard.png',
+              background : '/images/harvardbuilding.png'
+            },
+            {
+              image: '/images/UofU.png',
+              background : '/images/uofubuilding.jpeg'
+            },
+            {
+              image: '/images/MIT.png',
+              background : '/images/mitbuilding.jpeg'
+            },
+            {
+              image: '/images/Stanford.png',
+              background : '/images/stanfordbuilding.jpeg'
+            },
+            {
+              image: '/images/UofW.png',
+              background : '/images/uofwbuilding.jpeg'
+            },
+          ]
+        });
+      });
 }
