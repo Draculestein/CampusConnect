70a8f62ec6e3387602c8805fd3a17f3536c01cc0
Albert Julio <albertjuliotheodorus@gmail.com>
2024-02-15 13:52:07 -0700

    Refactored homeRouter

--- routes/home.ts
+++ routes/home.ts
@@ -3,10 +3,10 @@ import { Router } from "express";
 const homeRouter = Router();
 
 // define a route handler for the default home page
-    homeRouter.get("/", (req, res) => {
-        // render the index template
-        res.render("index", { title: "Hello World!" });
-    });
+homeRouter.get("/", (req, res) => {
+    // render the index template
+    res.render("index", { title: "Hello World!" });
+});
 
 
 export default homeRouter;
