0411375e8dd2ab2796174afadac3fc4d077e1790
Albert Julio <albertjuliotheodorus@gmail.com>
2024-02-15 23:40:48 -0700

    Adde sqlite config

---
+++ db.ts
@@ -0,0 +1,14 @@
+import { DataSource } from "typeorm";
+
+const AppDataSource = new DataSource({
+    type: 'sqlite',
+    database: 'dev.sqlite'
+})
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
