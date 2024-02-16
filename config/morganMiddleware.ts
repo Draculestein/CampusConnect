23b71f968b3f503652a6de3189323ff170bb9c12
Albert Julio <albertjuliotheodorus@gmail.com>
2024-02-16 10:41:53 -0700

    Removed skip logic

--- config/morganMiddleware.ts
+++ config/morganMiddleware.ts
@@ -1,22 +1,12 @@
-import morgan, { StreamOptions } from "morgan";
+import morgan, { StreamOptions } from 'morgan';
 
-import logger from "./logger";
+import logger from './logger';
 
 // Override the stream method by telling
 // Morgan to use our custom logger instead of the console.log.
 const stream: StreamOptions = {
   // Use the http severity
   write: (message) => logger.http(message),
-};
-
-// Skip all the Morgan http log if the
-// application is not running in development mode.
-// This method is not really needed here since
-// we already told to the logger that it should print
-// only warning and error messages in production.
-const skip = () => {
-  const env = process.env.NODE_ENV || "development";
-  return env !== "development";
 };
 
 // Build the morgan middleware
@@ -28,7 +18,7 @@ const morganMiddleware = morgan(
   ":method :url :status :res[content-length] - :response-time ms",
   // Options: in this case, I overwrote the stream and the skip logic.
   // See the methods above.
-  { stream, skip }
+  { stream }
 );
 
 export default morganMiddleware;
