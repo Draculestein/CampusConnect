a3d0968e07e2bdf7937b0e41fedae0b664a23bbc
Albert Julio <albertjuliotheodorus@gmail.com>
2024-05-17 16:17:28 -0600

    Removed firebase configs

--- config/firebase-backend.ts
+++
@@ -1,9 +0,0 @@
-import * as admin from 'firebase-admin';
-
-const serviceAccountKey = require('../thinkspace-d7f77-firebase-adminsdk-wvoz9-4260183b3c.json');
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountKey)
-})
-
-export default admin;
