3cc55fa819f4ea9bca4e86f4168d4952166ae52f
Albert Julio <albertjuliotheodorus@gmail.com>
2024-05-21 22:38:36 -0600

    Added sign in api route

--- routes/api.route.ts
+++ routes/api.route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { signUpWithEmailAndPassword } from '../controllers/auth.controllers';
+import { signInWithEmailAndPassword, signUpWithEmailAndPassword } from '../controllers/auth.controllers';
 
 const apiRouter = Router();
 
-// apiRouter.post('/login', async (req, res, next) => {
-
-// });
+apiRouter.post('/login', async (req, res, next) => {
+    signInWithEmailAndPassword(req, res);
+});
 
 apiRouter.post('/signup', (req, res, next) => {
     signUpWithEmailAndPassword(req, res);
