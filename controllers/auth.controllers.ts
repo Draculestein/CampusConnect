595e60478f300d9d886db26bc2676baab295ba96
Albert Julio <albertjuliotheodorus@gmail.com>
2024-05-22 19:12:25 -0600

    Added jwt to signin

--- controllers/auth.controllers.ts
+++ controllers/auth.controllers.ts
@@ -1,28 +1,40 @@
 import { Request, Response } from 'express';
-// import * as jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import { User } from '../db/models/User.model';
 import * as argon2 from 'argon2';
 import { UserRepository } from '../db/repositories/User.repositories';
 import logger from '../config/logger';
 
+const jwtSecret = process.env.JWT_SECRET;
+
 export async function signUpWithEmailAndPassword(req: Request, res: Response) {
     const { firstName, lastName, email, password } = req.body;
 
-    logger.debug('In signup controller');
-    const newUser = new User();
-    newUser.firstName = firstName;
-    newUser.lastName = lastName;
-    newUser.email = email;
-    newUser.password = await argon2.hash(password);
-
     try {
+        const result = await UserRepository.findOne({
+            where: {
+                email
+            }
+        });
+
+        if (result != null) {
+            res.status(403).json({ message: 'Email is already associated with an account' });
+            return;
+        }
+
+        const newUser = new User();
+        newUser.firstName = firstName;
+        newUser.lastName = lastName;
+        newUser.email = email;
+        newUser.password = await argon2.hash(password);
+
         await UserRepository.save(newUser);
 
-        res.status(200).send('Successful sign up with email and password');
+        res.status(200).json({ message: 'Successful sign up with email and password' });
     } catch (error) {
         logger.error(error);
-        res.status(500).send('Error signing up with email and password');
-    }
+        res.status(500).json({ message: 'Error signing up with email and password' });
+    };
 
 }
 
@@ -37,20 +49,32 @@ export async function signInWithEmailAndPassword(req: Request, res: Response) {
         });
 
         if (result == null) {
-            res.status(401).send('Unauthenticated');
+            res.status(401).json({ message: 'Unauthenticated' });
             return;
         }
 
         const verifyResult = await argon2.verify(result?.password!, password);
 
-        if (verifyResult)
-            res.status(200).send('Login successful');
+        if (verifyResult) {
+            const accessToken = jwt.sign(
+                {
+                    id: result.uuid
+                },
+                jwtSecret!,
+                {
+                    expiresIn: 86400
+                });
+            res.status(200).json({ 
+                message: 'Login successful',
+                accessToken  
+            });
+            return;
+        }
         else
-            res.status(401).send('Unauthenticated');
+            res.status(401).json({ message: 'Unauthenticated' });
     } catch (error) {
         logger.error(error);
-        res.status(500).send('Error signing in with email and password');
+        res.status(500).json({ message: 'Error signing in with email and password' });
 
-    }
-
+    };
 }
