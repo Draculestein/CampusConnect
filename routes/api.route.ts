9551198fdf9b3e6d0495eb861e39a215d448e695
Albert Julio <albertjuliotheodorus@gmail.com>
2024-05-26 16:36:36 -0600

    Added API login checks

--- routes/api.route.ts
+++ routes/api.route.ts
@@ -1,21 +1,26 @@
 import { Router } from "express";
-import { signInWithEmailAndPassword, signUpWithEmailAndPassword } from '../controllers/auth.controllers';
+import { signUpWithEmailAndPassword } from '../controllers/auth.controllers';
 import passport from "passport";
+import logger from "../config/logger";
 
 const apiRouter = Router();
 
-apiRouter.post('/login',
-    // async (req, res, next) => {
-    //     signInWithEmailAndPassword(req, res);
-    // }
-    passport.authenticate('local-signin', {
-        successRedirect: '/',
-        failureRedirect: '/login'
-    })
+apiRouter.post('/login', passport.authenticate('local-signin'),
+    (req, res, next) => {
+        if(!req.user) return res.status(401).json({ message: 'Unauthenticated!' });
+        // return res.status(200).json({ message: 'User authenticated' });
+        return res.redirect('/');
+    }
 );
 
-apiRouter.post('/signup', (req, res, next) => {
-    signUpWithEmailAndPassword(req, res);
+apiRouter.post('/signup', passport.authenticate('local-signin'), (req, res, next) => {
+    if(!req.user) return signUpWithEmailAndPassword(req, res);
+
+    return res.status(200);
+});
+
+apiRouter.post('/logout', (req, res, next) => {
+    req.logout((err) => logger.error(err));
 });
 
 export default apiRouter;
