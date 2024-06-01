27b11b70c958a7cbda341c6cfd1f9e1e60bc9540
Albert Julio <albertjuliotheodorus@gmail.com>
2024-05-31 21:16:58 -0600

    Changed searchByName, added searchByFilter

--- controllers/search.controllers.ts
+++ controllers/search.controllers.ts
@@ -1,19 +1,59 @@
+import { OrganizationSearch } from '../db/models/OrganizationSearch.model';
 import logger from '../config/logger';
 import { OrganizationSearchRepository } from '../db/repositories/OrganizationSearch.repositories';
 
 const numberPerPage = 5;
 
-export async function searchByName(name: string, page: number) {
+export async function searchByName(name: string) {
     try {
         const result = await OrganizationSearchRepository
             .createQueryBuilder('orgSearch')
             .innerJoin('orgSearch.id', 'org')
             .where('org.name = :name', { name })
-            .skip(numberPerPage * (page - 1))
-            .take(5);
+            .getMany();
 
-        return result;
+        const resultTuple: [OrganizationSearch[], any] = [result, null];
+
+        return resultTuple;
     } catch (error) {
         logger.error('Search by name error:\n' + error);
+        const resultTuple: [null, any] = [null, error];
+
+        return resultTuple;
+    }
+}
+
+export async function searchByFilters(
+    page: number,
+    numOfYears?: string,
+    cityType?: string,
+    climate?: string,
+    isPublic?: boolean,
+    country?: string,
+) {
+
+    try {
+        let where = {};
+        where = {
+            ...where,
+            ...(numOfYears && { numOfYears }),
+            ...(cityType && { cityType }),
+            ...(climate && { climate }),
+            ...(isPublic && { isPublic }),
+            ...(country && { country })
+        }
+
+        const result = await OrganizationSearchRepository.findAndCount({
+            where,
+            skip: numberPerPage * page,
+            take: numberPerPage
+        });
+
+        const resultTuple: [OrganizationSearch[], number, any] = [result[0], result[1], null];
+        return resultTuple;
+    } catch (error) {
+        logger.error('Search by filter error:\n' + error);
+        const resultTuple: [null, number, any] = [null, 0, error];
+        return resultTuple;
     }
 }
