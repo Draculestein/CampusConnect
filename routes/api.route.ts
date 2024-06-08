a480568e3956fac079d61c5f55c20bf7d73b27d9
Albert Julio <albertjuliotheodorus@gmail.com>
2024-06-07 21:26:41 -0600

    Added temporary route for applying in API. Also, refactor format

--- routes/api.route.ts
+++ routes/api.route.ts
@@ -3,22 +3,22 @@ import { signUpWithEmailAndPassword } from '../controllers/auth.controllers';
 import { searchByFilters, searchByName } from '../controllers/search.controllers';
 import passport from "passport";
 import logger from "../config/logger";
+import { expectLogin } from "../middleware/auth.middleware";
 
 const apiRouter = Router();
 
 apiRouter.post('/login', passport.authenticate('local-signin'),
     (req, res, next) => {
-        if(!req.user) return res.status(401).json({ message: 'Unauthenticated!' });
+        if (!req.user) return res.status(401).json({ message: 'Unauthenticated!' });
         const redirect = req.cookies.redirect || '/';
-        logger.debug('Redirect:' + redirect);
-        // return res.status(200).json({ message: 'User authenticated' });
+
         res.clearCookie('redirect');
         return res.redirect(redirect);
     }
 );
 
 apiRouter.post('/signup', (req, res, next) => {
-    if(!req.user) return signUpWithEmailAndPassword(req, res);
+    if (!req.user) return signUpWithEmailAndPassword(req, res);
 
     return res.status(200);
 });
@@ -30,42 +30,42 @@ apiRouter.post('/logout', (req, res, next) => {
 apiRouter.post('/search-filter', async (req, res, next) => {
     let page = 1;
     let numOfYears = null;
-    let cityType =  null;
+    let cityType = null;
     let climate = null;
     let isPublic = null;
     let country = null;
 
     // Input checking
 
-    if(req.body.page) {
+    if (req.body.page) {
         page = Number(req.body.page);
     }
 
-    if(req.body.program && (req.body.program === 'two' || req.body.program === 'four')) {
+    if (req.body.program && (req.body.program === 'two' || req.body.program === 'four')) {
         numOfYears = req.body.program === 'two' ? 2 : 4;
     }
 
-    if(req.body.cityType) {
+    if (req.body.cityType) {
         cityType = String(req.body.cityType);
     }
 
-    if(req.body.climate) {
+    if (req.body.climate) {
         climate = String(req.body.climate);
     }
 
-    if(req.body.isPublic) {
+    if (req.body.isPublic) {
         isPublic = Boolean(isPublic);
     }
 
-    if(req.body.country) {
+    if (req.body.country) {
         country = String(req.body.country);
     }
 
     const [resultArray, resultCount, error] = await searchByFilters(page, numOfYears, cityType, climate, isPublic, country);
 
-    logger.info(String(page) + ' ' + numOfYears+ ' ' + cityType+ ' ' + climate+ ' ' + isPublic+ ' ' + country);
+    logger.info(String(page) + ' ' + numOfYears + ' ' + cityType + ' ' + climate + ' ' + isPublic + ' ' + country);
 
-    if(error)
+    if (error)
         return next(error);
 
     return res.status(200).json({
@@ -75,15 +75,23 @@ apiRouter.post('/search-filter', async (req, res, next) => {
 })
 
 apiRouter.post('/search-name', async (req, res) => {
-    if(!req.body.name)
-        return res.sendStatus(404).json({ message: 'No name property!'});
+    if (!req.body.name)
+        return res.sendStatus(404).json({ message: 'No name property!' });
 
     const [resultArray, error] = await searchByName(req.body.name);
 
-    if(error) return res.status(404).json({ message: 'Error! ' + error});
+    if (error) return res.status(404).json({ message: 'Error! ' + error });
 
     return res.status(200).json(
         resultArray
     );
+});
+
+apiRouter.post('/apply/:orgUrl', expectLogin, async (req, res) => {
+    if (!req.user) return res.status(401).json({ message: 'Unauthorized!' });
+
+    const userID = req.user.id;
+
+    logger.info(req.body);
 });
 export default apiRouter;
