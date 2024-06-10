beb4b300248c4b27cd617ba4754f7e8755bbf908
Albert Julio <albertjuliotheodorus@gmail.com>
2024-06-10 15:56:18 -0600

    Fixed size limit to 10MB

--- server.ts
+++ server.ts
@@ -23,7 +23,7 @@ async function runExpress() {
     app.set("view engine", "ejs");
 
     app.use(morganMiddleware);
-    app.use(express.json({limit: '50mb'}));
+    app.use(express.json({limit: '10mb'}));
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
 
