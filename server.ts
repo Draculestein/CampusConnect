9d80dca5069ffa845e094608f580a87eca107c0d
Albert Julio <albertjuliotheodorus@gmail.com>
2024-06-09 22:41:52 -0600

    Server now ony starts if database and bucket connections are succesful

--- server.ts
+++ server.ts
@@ -13,73 +13,70 @@ import passport from "passport";
 import { runTestData } from './db/testData/runTestData';
 import { initializeBucket } from './s3/s3';
 
-let successfulSetup = true;
 
-AppDataSource.initialize()
-    .then(() => {
-        logger.info("Data Source has been initialized!");
-        if (process.env.DEV_MODE! === '1') runTestData();
-    })
-    .catch((err) => {
-        logger.error("Error during Data Source initialization: " + err);
-        successfulSetup = successfulSetup && false;
+async function runExpress() {
+    const app = express();
+    const port = process.env.PORT || 3000; // default port to listen
+
+    // Configure Express to use EJS
+    app.set("views", path.join(__dirname, "views"));
+    app.set("view engine", "ejs");
+
+    app.use(morganMiddleware);
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
+
+    app.use(session({
+        secret: process.env.SESSION_SECRET!,
+        resave: false,
+        saveUninitialized: false,
+        store: new TypeormStore({
+            cleanupLimit: 2,
+            limitSubquery: false, // If using MariaDB.
+            ttl: 86400
+        }).connect(SessionRepository)
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    app.use(express.static("public"));
+
+    registerRoutes(app);
+
+    // start the express server
+    app.listen(port, () => {
+        // tslint:disable-next-line:no-console
+        logger.info(`Server started at http://localhost:${port}`);
     });
-
-initializeBucket()
-    .then(([isSuccessful, error]) => {
-        if (isSuccessful) { 
-            logger.info('Bucket is ready!'); 
-            return;
-        }
-
-        logger.error('Error when initializing bucket: ' + error);
-        successfulSetup = successfulSetup && false;
-    })
-    .catch((error) => {
-        logger.error('Error when initializing bucket: ' + error);
-        successfulSetup = successfulSetup && false;
-
-    })
-
-if(!successfulSetup) {
-    logger.error('Fail to setup connection to database and/or bucket! Aborting Express!');
-    process.exit();
 }
 
-const app = express();
-const port = process.env.PORT || 3000; // default port to listen
+async function bootstrap() {
+    let successfulSetup = true;
 
-// Configure Express to use EJS
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+    try {
+        await AppDataSource.initialize();
+        logger.info("Data Source has been initialized!");
+        if (process.env.DEV_MODE! === '1') runTestData();
+    } catch (error) {
+        logger.error("Error during Data Source initialization: " + error);
+        successfulSetup = successfulSetup && false;
+    }
+    const [isSuccessful, error] = await initializeBucket();
+    if (isSuccessful) {
+        logger.info('Bucket is ready!');
+    } else {
+        logger.error('Error when initializing bucket: ' + error);
+        successfulSetup = successfulSetup && false;
+    }
 
-app.use(morganMiddleware);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+    if (!successfulSetup) {
+        logger.error('Fail to setup connection to database and/or bucket! Aborting Express!');
+        process.exit();
+    }
 
-app.use(session({
-    secret: process.env.SESSION_SECRET!,
-    resave: false,
-    saveUninitialized: false,
-    store: new TypeormStore({
-        cleanupLimit: 2,
-        limitSubquery: false, // If using MariaDB.
-        ttl: 86400
-    }).connect(SessionRepository)
-}));
+    runExpress();
+}
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(express.static("public"));
-
-registerRoutes(app);
-
-
-
-// start the express server
-app.listen(port, () => {
-    // tslint:disable-next-line:no-console
-    logger.info(`Server started at http://localhost:${port}`);
-});
+bootstrap();
